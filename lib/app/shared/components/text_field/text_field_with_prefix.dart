4b85492a5642124d275e9d71947480bde90872fa
Gabrielasfv <gabrielasantosfv@gmail.com>
2022-08-12 23:29:31 -0300

    add text field components

---
+++ lib/app/shared/components/text_field/text_field_with_prefix.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'decoration.dart';
+
+class TextFieldWithPrefix extends StatelessWidget {
+  final TextEditingController controller;
+  //final String? Function(String?)? validator;
+  final String hintText;
+  final IconData prefixIcon;
+  final bool obscureText;
+  final TextInputType keyboardType;
+
+  const TextFieldWithPrefix({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.prefixIcon,
+    required this.obscureText,
+    //required this.validator,
+    this.keyboardType = TextInputType.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: style,
+      controller: controller,
+      keyboardType: keyboardType,
+      //validator: validator,
+      decoration: InputDecoration(
+        prefixIcon: icon(prefixIcon),
+        errorStyle: errorStyle,
+        hintStyle: hintStyle,
+        hintText: hintText,
+        enabledBorder: inputBorder,
+        focusedBorder: inputBorder,
+        filled: true,
+        fillColor: const Color(0xfff2f2f2),
+      ),
+      obscureText: obscureText,
+    );
+  }
+}
---
+++ lib/app/shared/components/text_field/text_field_with_suffix.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'decoration.dart';
+
+class TextFieldWithSuffix extends StatelessWidget {
+  final TextEditingController controller;
+  //final String? Function(String?)? validator;
+  final String hintText;
+  final IconData suffixIcon;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final void Function()? onTapSuffixIcon;
+
+  const TextFieldWithSuffix({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.suffixIcon,
+    required this.obscureText,
+    //required this.validator,
+    this.keyboardType = TextInputType.text,
+    this.onTapSuffixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: style,
+      controller: controller,
+      keyboardType: keyboardType,
+      //validator: validator,
+      decoration: InputDecoration(
+        suffixIcon: GestureDetector(
+          onTap: onTapSuffixIcon,
+          child: icon(suffixIcon),
+        ),
+        errorStyle: errorStyle,
+        hintStyle: hintStyle,
+        hintText: hintText,
+        enabledBorder: inputBorder,
+        focusedBorder: inputBorder,
+        filled: true,
+        fillColor: const Color(0xfff2f2f2),
+      ),
+      obscureText: obscureText,
+    );
+  }
+}
