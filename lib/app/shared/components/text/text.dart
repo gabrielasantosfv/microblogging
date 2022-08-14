7873e6cb859ff99a2ed7dd60809650d12c11cbe2
Gabrielasfv <gabrielasantosfv@gmail.com>
2022-08-14 20:46:41 -0300

    text component

---
+++ lib/app/shared/components/text/text.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TextComponent extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+  final FontWeight fontWeight;
+  final TextAlign textAlign;
+  final String fontFamily;
+
+  const TextComponent({
+    Key? key,
+    required this.text,
+    this.fontSize = 16,
+    this.color = const Color(0xff3b5168),
+    this.fontWeight = FontWeight.normal,
+    this.textAlign = TextAlign.center,
+    this.fontFamily = 'Lato',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+        fontFamily: fontFamily,
+      ),
+      textAlign: textAlign,
+    );
+  }
+}
