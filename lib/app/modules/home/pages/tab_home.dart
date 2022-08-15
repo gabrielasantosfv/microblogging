3a0a5bf936000394f3075899953f095077e963c8
Gabrielasfv <gabrielasantosfv@gmail.com>
2022-08-14 23:05:24 -0300

    add home tabs page

---
+++ lib/app/modules/home/pages/tab_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabHomePage extends StatefulWidget {
+  const TabHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TabHomePage> createState() => _TabHomePageState();
+}
+
+class _TabHomePageState extends State<TabHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.amber),
+    );
+  }
+}
---
+++ lib/app/modules/home/pages/tab_new_post.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabNewPostPage extends StatefulWidget {
+  const TabNewPostPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabNewPostPage> createState() => _TabNewPostPageState();
+}
+
+class _TabNewPostPageState extends State<TabNewPostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.purple),
+    );
+  }
+}
