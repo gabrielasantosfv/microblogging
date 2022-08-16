294912f0a9695c8b29cce7295c4fa52c79c0b186
Gabrielasfv <gabrielasantosfv@gmail.com>
2022-08-16 19:20:25 -0300

    call service on home screens

--- lib/app/modules/home/pages/tab_latest_news.dart
+++ lib/app/modules/home/pages/tab_latest_news.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:microblogging/app/modules/home/home_controller.dart';
 import 'package:microblogging/app/shared/components/card/card.dart';
+import 'package:microblogging/app/shared/components/text/text.dart';
+import 'package:microblogging/app/shared/utils/date_format.dart';
 
 class TabLatestNewsPage extends StatefulWidget {
   const TabLatestNewsPage({Key? key}) : super(key: key);
@@ -9,31 +13,70 @@ class TabLatestNewsPage extends StatefulWidget {
 }
 
 class _TabLatestNewsPageState extends State<TabLatestNewsPage> {
+  final _homeController = HomeController();
+
+  @override
+  void initState() {
+    _homeController.getListNews(context);
+    super.initState();
+  }
+
+  Future<void> onRefresh() async {
+    if (!_homeController.progressListNews) {
+      _homeController.updatingListNews = true;
+      await _homeController.getListNews(context);
+      _homeController.updatingListNews = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Novidades')),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => const Divider(),
-        itemCount: 5,
-        shrinkWrap: true,
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(10),
-        physics: const ClampingScrollPhysics(),
-        itemBuilder: (context, index) {
-          return const CardComponent(
-            name: 'O Boticário',
-            date: '14/08/2022',
-            image:
-                'https://gb-mobile-app-teste.s3.amazonaws.com/oboticario.png',
-            text:
-                'Além disso, nossos funcionários e familiares receberão kits de proteção. Afinal, o cuidado começa aqui dentro, né?',
-            numberLikes: 67,
-            numberComments: 15,
-            verified: true,
-          );
-        },
+      body: RefreshIndicator(
+        color: const Color(0xff3b5168),
+        onRefresh: () => onRefresh(),
+        child: Observer(builder: (_) {
+          return !_homeController.progressListNews
+              ? _homeController.listNews.isNotEmpty
+                  ? list()
+                  : Center(
+                      child: TextComponent(
+                        text: _homeController.updatingListNews
+                            ? 'Buscando novos dados...'
+                            : 'Nenhum dado foi encontrado.',
+                      ),
+                    )
+              : const Center(
+                  child: CircularProgressIndicator(color: Color(0xff3b5168)),
+                );
+        }),
       ),
+    );
+  }
+
+  Widget list() {
+    return ListView.separated(
+      separatorBuilder: (context, index) => const Divider(),
+      itemCount: _homeController.listNews.length,
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.all(10),
+      physics: const ClampingScrollPhysics(),
+      itemBuilder: (context, index) {
+        return CardComponent(
+          name: _homeController.listNews[index].user!.name,
+          date: dateTimeFormat(
+            _homeController.listNews[index].message!.createdAt,
+          ),
+          image: _homeController.listNews[index].user!.profilePicture,
+          text: _homeController.listNews[index].message!.content,
+          numberLikes: _homeController.listNews[index].message!.numberLikes,
+          numberComments:
+              _homeController.listNews[index].message!.numberComments,
+          verified: _homeController.listNews[index].user!.verified,
+        );
+      },
     );
   }
 }
